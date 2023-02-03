0cf576e68cef31c04cc5451a1f4449069f766d28
quanglinh <linhdqph27370@fpt.edu.vn>
2023-02-04 00:09:42 +0700

    done 111

---
+++ section 9 Data structures , Modern Operators and Strings/Looping_Arrays_The_for_of_Loop_111..js
@@ -0,0 +1,53 @@
+'use strict';
+const restaurant ={
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenza, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian','Organic'],
+    starterMenu: ['Focaccia','Bruschetta','Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza','Pasta','Risotto'],
+    openingHours: {
+        thu:{
+            open: 12,
+            close: 22,
+        },
+        fri: {
+            open: 11,
+            close: 13,
+        },
+        sat: {
+            open: 0,
+            close: 21,
+        },  
+    },
+    order: function(starterIndex,mainIndex){
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    },
+    oderDeLivery: function({starterIndex =1,
+         mainIndex = 0,
+         time = '20:20',
+         address}){
+      console.log(`Order recived! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+    },
+    orderPasta: function(ing1, ing2 ,ing3){
+        console.log(`Here is your declicious past with ${ing1}, ${ing2} and ${ing3}`)
+    },
+    orderPizza: function(mainIngredient, ...ortherIngredients){
+        console.log(mainIngredient);
+        console.log(ortherIngredients);
+    }
+};
+const menu =[...restaurant.starterMenu, ...restaurant.mainMenu];
+
+for(const item of menu) console.log(item);
+
+for(const [i, el] of menu.entries()){
+    console.log(`${i} : ${el}`)
+}
+
+
+// old school kieu cu
+// for(const item of menu.entries()){
+//     console.log(`${item[0] +1} : ${item[1]}`)
+// }
+
+// console.log([...menu.entries()])
